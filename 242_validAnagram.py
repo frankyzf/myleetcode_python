0cf3f10e710b81c7fd4a5f2ca5a5539434b75b46
frankyzf <frankyzf@gmail.com>
2015-11-29 21:26:58 +0800

    a += 1

---
+++ 242_validAnagram.py
@@ -0,0 +1,18 @@
+__name__ = 'feng'
+from collections import Counter
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        print Counter(s).items()
+        return Counter(s).items() == Counter(t).items()
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.isAnagram("abc", "bac")
