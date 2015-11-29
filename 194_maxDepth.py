b21264bcc3a561b2f4acef5015934b7862b15744
frankyzf <frankyzf@gmail.com>
2015-11-29 19:07:21 +0800

    104 recursive cal

---
+++ 194_maxDepth.py
@@ -0,0 +1,23 @@
+__name__ = 'feng'
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+
+
+if __name__ == '__main__':
+    s = Solution()
