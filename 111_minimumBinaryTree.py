fc2db991e378be4fbdf47eb46af5ab8c3701cf6d
frankyzf <frankyzf@gmail.com>
2015-12-02 22:00:12 +0800

    set

---
+++ 111_minimumBinaryTree.py
@@ -0,0 +1,28 @@
+__author__ = 'feng'
+
+class TreeNode(object):
+   def __init__(self, x):
+       self.val = x
+       self.left = None
+       self.right = None
+
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        if root.left and root.right:
+            return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+        elif root.left:
+            return self.minDepth(root.left) + 1
+        elif root.right:
+            return self.minDepth(root.right) + 1
+        return 1
+
+
+
+if __name__ == '__main__':
+    s = Solution()
