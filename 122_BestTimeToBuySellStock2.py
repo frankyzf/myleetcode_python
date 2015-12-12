0b6ee2db13d3f22d7fb0e66d8029ee1fec65f5f8
frankyzf <frankyzf@gmail.com>
2015-12-12 13:31:13 +0800

    easy

---
+++ 122_BestTimeToBuySellStock2.py
@@ -0,0 +1,12 @@
+__author__ = 'feng'
+
+from util import *
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+
+if __name__ == '__main__':
+    s = Solution()
