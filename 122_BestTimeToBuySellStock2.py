928282030d402199ae07cc1a0555ee1c0832595e
frankyzf <frankyzf@gmail.com>
2015-12-12 13:38:35 +0800

    easy

--- 122_BestTimeToBuySellStock2.py
+++ 122_BestTimeToBuySellStock2.py
@@ -7,6 +7,22 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        if len(prices) < 2:
+            return 0
+        i = 1
+        while i < len(prices) and prices[i] < prices[i-1]:
+            i += 1
+        begin = i -1
+        v = 0
+        while i < len(prices):
+            begin = i -1
+            while i < len(prices) and prices[i] >= prices[i-1]:
+                i += 1
+            v += prices[i-1] - prices[begin]
+            while i < len(prices) and prices[i] < prices[i-1]:
+                i += 1
+        return v
+
 
 if __name__ == '__main__':
     s = Solution()
